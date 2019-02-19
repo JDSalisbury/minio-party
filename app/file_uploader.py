706656f2af3fc6322884bd58ed5ce7c8d347662f
Jeff Salisbury <Salisbury.jeffery@gmail.com>
2019-02-19 10:48:22 -0500

    The journey has been this, Installed minio crap, Didnt like it. Installed boto3, can now upload files and download... kinda, My pc is not allowing it. Then we move onto django-storage which is mainly for AWS S3 setup, but I was told minio can just tag along and be used instead. so everything will say AWS this and AWS that but ill be using minio i guess.

--- app/file_uploader.py
+++ app/file_uploader.py
@@ -1,25 +1,32 @@
-from minio import Minio
-from minio.error import ResponseError
-
-minioClient = Minio('play.minio.io:9000',
-                    access_key='NRY1PGXB4K5N8UK9XORC',
-                    secret_key='uUQtcMEoX7phtXEkmvSX+C2wH7kHNaZHm600cUKh',
-                    secure=False)
+import boto3
+from botocore.client import Config
 
 
-# Make a bucket with the make_bucket API call.
-try:
-    minioClient.make_bucket("maylogs", location="us-east-1")
-except BucketAlreadyOwnedByYou as err:
-    pass
-except BucketAlreadyExists as err:
-    pass
-except ResponseError as err:
-    raise
-else:
-    # Put object 'pumaserver_debug.log' w/ content from 'pumaserver_debug.log'.
-    try:
-        minioClient.fput_object(
-            'maylogs', 'pumaserver_debug.log', '/tmp/pumaserver_debug.log')
-    except ResponseError as err:
-        print(err)
+s3 = boto3.resource(
+    's3',
+    endpoint_url='http://localhost:9000',
+    aws_access_key_id='NRY1PGXB4K5N8UK9XORC',
+    aws_secret_access_key='uUQtcMEoX7phtXEkmvSX+C2wH7kHNaZHm600cUKh',
+    config=Config(signature_version='s3v4'),
+    region_name='us-east-1'
+)
+
+for bucket in s3.buckets.all():
+    print(bucket.name)
+
+FILE_LOCATION = r'C:\Users\jsalisbury\Downloads\Glitch7NA.mp3'
+DOWNLOAD_LOCATION = r'C:\Users\jsalisbury\Downloads\TestLocation'
+
+# uploadfile from local file system to bucket 'songs'.
+s3.Bucket('local-test-media').upload_file(
+    FILE_LOCATION,
+    'Glitch7NA.mp3'
+)
+
+# download the object from the bucket 'songs' and save it to local
+s3.Bucket('local-test-media').download_file(
+    'Glitch7NA.mp3', DOWNLOAD_LOCATION)
+
+print(
+    "Downloaded '[HQ] The Glitch Mob - Seven Nation Army Remi.mp3' as  'classical.mp3'. "
+)
