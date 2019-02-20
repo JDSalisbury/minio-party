c39816d532f9b3b22cdbf322563f46faf962fe94
Jeff Salisbury <Salisbury.jeffery@gmail.com>
2019-02-20 15:53:31 -0500

    Was able to set up the save to work via admin, also admin add the file structure in. So post is without the file structure set up, The file structure is messed up tho, it saves in media/app/media and is kinda blah, but may be alright.

--- storage/core/models.py
+++ storage/core/models.py
@@ -4,11 +4,24 @@ import uuid
 
 
 def file_path(instance, filename):
+    name = filename.split('.')[0]
     ext = filename.split('.')[-1]
-    filename = f'{uuid.uuid4}.{ext}'
+    filename = f'{uuid.uuid4}--{name}.{ext}'
     return 'app/' + settings.MEDIA_URL + filename
+
+
+def minioBucket(self):
+    settings.S3_BUCKET.put_object(
+        ACL='public-read-write',
+        Key=self.file.name,
+        Body=self.file
+    )
 
 
 class Document(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
     file = models.FileField(upload_to=file_path)
+
+    def save(self, *args, **kwargs):
+        super(Document, self).save(*args, **kwargs)
+        minioBucket(self)
